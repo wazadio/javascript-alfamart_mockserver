2003baf275c831eececd83c5bb1915ce2c8636dd
wazadio <junidio306@gmail.com>
2021-01-27 18:39:04 +0700

    update respon

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 const invalid_password = "mock_server"
+const expired_password = "123"
 const mobile_number = "081234566789"
 const current_amount = 100000
 const amount_limit = 200000
 const min_amount_limit = 10000
 var respon
-const transaction_type = ["111", "114"]
+const transaction_type = ["111", "112", "114"]
 const parameter = ['login', 'pwd', 'tx_date', 'tx_type', 'terminal', 'cust_key', 'tx_amt', 'tx_id', 'name', 'rek_type']
+const blacklisted = "123456789"
+
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
@@ -26,6 +29,9 @@ server.get('/request', (req, res) => {
   else if (req_param.pwd != invalid_password){
   	res.status(400).send("0004:Invalid PIN")
   }
+  else if (req_param.pwd == expired_password){
+    res.status(400).send("00Q1:PIN Expired")
+  }
   else if (transaction_type.includes(req_param.tx_type) == false) {
     res.status(400).send("0005:Invalid Transaction Type")
   }
@@ -34,6 +40,9 @@ server.get('/request', (req, res) => {
   }
   else if (req_param.cust_key != mobile_number) {
     res.status(400).send("00LH:Invalid Phone Number")
+  }
+  else if (req_param.cust_key == blacklisted) {
+    res.status(400).send("00T7:Customer Blacklisted")
   }
   else if (isNaN(Number(req_param.tx_amt)) != true) {
     if (req_param.tx_type == "111") {
@@ -48,6 +57,15 @@ server.get('/request', (req, res) => {
     else if (req_param.tx_type == "114") {
       if ((current_amount - tx_amt) < min_amount_limit) {
         res.status(400).send("0014:Exceed Withdrawal Limit")
+      }
+      else {
+       respon = "0000"+":"+"Succes"+":"+req_param.tx_id+":"+req_param.tx_date+":"+req_param.name+":"+req_param.rek_type+":"+"1000"+":"+req_param.cust_key
+       res.send(respon)
+      }
+    }
+    else if (req_param.tx_type == "112") {
+      if ((current_amount - tx_amt) < min_amount_limit) {
+        res.status(400).send("0015:Exceed # Limit")
       }
       else {
        respon = "0000"+":"+"Succes"+":"+req_param.tx_id+":"+req_param.tx_date+":"+req_param.name+":"+req_param.rek_type+":"+"1000"+":"+req_param.cust_key
