a4f47697a3bd08dc481ad12067207705aef77fe3
wazadio <junidio306@gmail.com>
2021-02-02 16:12:13 +0700

    fixing port error 2

--- server.js
+++ server.js
@@ -100,6 +100,6 @@ server.use((req, res, next) => {
 */
 // Use default router
 //server.use(router)
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('JSON Server is running')
 })
